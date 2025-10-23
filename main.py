229cf6c18d2254ddaac3dcc3639f7045eacaf0ff
Maulik Rawal <maulikrawal@proton.me>
2025-10-23 21:41:18 +0000

    squash all commits

--- main.py
+++ main.py
@@ -6,31 +6,36 @@ while True:
     print("3 : Edit")
     print("4 : Complete")
     print("5 : Exit")
+    # Get user input and strip whitespace
     user_action = input("Enter a value: ")
     user_action = user_action.strip()
 
     match user_action:
-        case '1':
+        case '1': # Add
             todo = input("Enter a to do: ") + "\n"
 
+            # Read existing todos from file
             file = open("todos.txt", "r")
             todos = file.readlines()
             file.close()
 
+            # Add new todo to the list
             todos.append(todo)
 
+            # Write updated todos back to file
             txtfile = open("todos.txt", "a")
             txtfile.writelines(todo)
             txtfile.close()
-        case '2':
+        case '2': # Show
             file = open("todos.txt", "r")
             todos = file.readlines()
             file.close()
 
             for (i, item) in enumerate(todos):
+                item = item.strip('\n')
                 row = f"{i+1}.{item.capitalize()}"
                 print(row)
-        case '3':
+        case '3': # Edit
             for (i, item) in enumerate(todos):
                 row = f"{i+1}.{item.capitalize()}"
                 print(row)
@@ -41,9 +46,9 @@ while True:
             for (i, item) in enumerate(todos):
                 row = f"{i+1}.{item.capitalize()}"
                 print(row)
-        case '4':
+        case '4': # Complete
             number = int(input("Number of todo list to complete: "))
             todos.pop(number - 1)
-        case _ :
+        case _ : # Exit
             print("Exiting...")
             break
